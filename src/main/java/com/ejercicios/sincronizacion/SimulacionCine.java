32bd5565fd8e17fac0332ee13f2e34b31b7e6458
UnaxGainzarain <unaxarbizu1@gmail.com>
2026-01-29 11:12:35 +0100

    Runnable inicial implementado

--- src/main/java/com/ejercicios/sincronizacion/SimulacionCine.java
+++ src/main/java/com/ejercicios/sincronizacion/SimulacionCine.java
@@ -1,6 +1,16 @@
 package com.ejercicios.sincronizacion;
 
 public class SimulacionCine {
+	 static final int NUM_TAQUILLAS = 1;
+	    static final int TOTAL_ASIENTOS = 5;
+	    static final int NUM_COLAS = 4;           
+	    static final int MAX_PERSONAS_COLA = 5;  
+	    
+	    static final int TIEMPO_SIMULACION = 10000; 
+	    static final int TASA_LLEGADA_CLIENTES = 50; 
+	    
+	    public static final int TIEMPO_VENTA_MIN = 2000;
+	    public static final int TIEMPO_VENTA_MAX = 3000;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- src/main/java/com/ejercicios/sincronizacion/Taquilla.java
+++ src/main/java/com/ejercicios/sincronizacion/Taquilla.java
@@ -17,7 +17,27 @@ public class Taquilla implements Runnable{
 	
 	@Override
 	public void run() {
-		 
+        System.out.println(nombre + " abierta y esperando clientes.");
+        try {
+			Cliente cliente = cine.atenderCliente();
+			if(cliente !=null) {
+				System.out.println("Atendiendo al cliente " + cliente.getId());
+				 int tiempoVenta = generator.nextInt(
+	                        SimulacionCine.TIEMPO_VENTA_MAX - SimulacionCine.TIEMPO_VENTA_MIN + 1
+	                    ) + SimulacionCine.TIEMPO_VENTA_MIN;
+				 Thread.sleep(tiempoVenta);
+				 
+				 //Ahora intentamos finalizar la venta
+				 if (cine.intentarVenderEntrada()) {
+                     System.out.println( nombre + " ha vendido una entrada al Cliente " + cliente.getId());
+                 } else {
+                     System.out.println( nombre + " no pudo vender. CINE LLENO. Cliente " + cliente.getId() + " se queda sin entrada.");
+                 }
+
+			}
+		} catch (InterruptedException e) {
+			System.out.println(nombre + " cerrando caja (Interrupción recibida).");
+		}
 	}
 
 }
